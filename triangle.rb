b57e83351e405360dcc6c5a88a4a021fd34f123d
catheraaine <catheraaine@gmail.com>
2016-01-20 11:47:49 -0500

    Finished 'Triangle Problem'. Status 155/282

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if (a == b ) && (b == c) && (a == c)
+    return :equilateral
+
+  elsif (a == b) || (b == c) || (a == c)
+    return :isosceles
+
+  else
+    return :scalene
+
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
