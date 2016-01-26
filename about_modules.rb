63ddd0674e452b84dc7ca2445cade3bab777bae4
catheraaine <catheraaine@gmail.com>
2016-01-26 10:41:06 -0500

    Finish "Modules". Status 228/282

--- about_modules.rb
+++ about_modules.rb
@@ -11,8 +11,9 @@ class AboutModules < Neo::Koan
     end
   end
 
+# I am still not entirely sure why certain errors are chosen. Testing thing?
   def test_cant_instantiate_modules
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       Nameable.new
     end
   end
@@ -39,7 +40,7 @@ class AboutModules < Neo::Koan
 
   def test_normal_methods_are_available_in_the_object
     fido = Dog.new
-    assert_equal __, fido.bark
+    assert_equal "WOOF", fido.bark
   end
 
   def test_module_methods_are_also_available_in_the_object
@@ -51,13 +52,13 @@ class AboutModules < Neo::Koan
 
   def test_module_methods_can_affect_instance_variables_in_the_object
     fido = Dog.new
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
     fido.set_name("Rover")
-    assert_equal __, fido.name
+    assert_equal "Rover", fido.name
   end
 
   def test_classes_can_override_module_methods
     fido = Dog.new
-    assert_equal __, fido.here
+    assert_equal :in_object, fido.here
   end
 end
